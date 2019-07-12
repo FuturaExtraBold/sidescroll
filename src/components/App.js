1c294c70cc7290f290964abf451d5fa69d976eec
Ben Hays <ben@kajabi.com>
2019-07-12 14:20:52 -0700

    Getting scrolling to work.

--- src/components/App.js
+++ src/components/App.js
@@ -18,9 +18,31 @@ class App extends Component {
     });
     $slider.css("width", sliderWidth + "px");
 
-    function handleWheel(evt) {
+    function handleWheel(event) {
+
+      // var e = window.event || event;
+      // let delta = -e.wheelDelta || -e.detail;
+      // let newScrollTop = $window.scrollTop() + delta;
+      // $window.scrollTop(newScrollTop);
+      // console.log("newScrollTop:", newScrollTop);
+      // TweenMax.to($slider, 0.5, { x: -$window.scrollTop(), ease: "easeOutExpo" });
+
+      let newScrollTop = $window.scrollTop() + event.originalEvent.deltaX;
+      $window.scrollTop(newScrollTop);
+      console.log("newScrollTop:", newScrollTop);
       TweenMax.to($slider, 0.5, { x: -$window.scrollTop(), ease: "easeOutExpo" });
     }
+    $window.on("wheel", handleWheel);
+
+    // let addMouseWheelEventListener = function(scrollHandler) {
+    //   if (window.addEventListener) {
+    //     window.addEventListener("mousewheel", scrollHandler, false); // IE9+, Chrome, Safari, Opera
+    //     window.addEventListener("DOMMouseScroll", scrollHandler, false); // Firefox
+    //   } else {
+    //     window.attachEvent("onmousewheel", scrollHandler); // IE 6/7/8
+    //   }
+    // }
+    // addMouseWheelEventListener(handleWheel);
 
     function updateWindow() {
       windowWidth = $window.outerWidth();
@@ -28,10 +50,7 @@ class App extends Component {
       let holderHeight = sliderWidth - windowWidth + windowHeight;
       $holder.css("height", holderHeight + "px");
     }
-
-    $window.on("wheel", handleWheel);
     $window.on("resize", updateWindow);
-
     updateWindow();
   }
 
@@ -44,7 +63,9 @@ class App extends Component {
             <section className="section"></section>
             <section className="section"></section>
             <section className="section"></section>
-            <section className="section"></section>
+            <section className="section">
+              <h1 className="section__title">Kajabi does it all.</h1>
+            </section>
           </div>
         </div>
       </div>
