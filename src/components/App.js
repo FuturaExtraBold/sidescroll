bd03e7830861341e09f1ad95f61850083e7b90a1
Ben Hays <ben@kajabi.com>
2019-07-12 13:08:11 -0700

    Got window resizing working properly.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import $ from "jquery";
+import TweenMax from "gsap";
 
-function App() {
-  return (
-    <h1>App</h1>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    let $window = $(window);
+    let windowWidth, windowHeight;
+
+    let $holder = $(".holder");
+    let $slider = $(".slider");
+    let $sections = $(".section");
+
+    let sliderWidth = 0;
+    $sections.each((index) => {
+      sliderWidth += $sections.eq(index).outerWidth();
+    });
+    $slider.css("width", sliderWidth + "px");
+
+    function handleWheel(evt) {
+      TweenMax.to($slider, 0.5, { x: -$window.scrollTop(), ease: "easeOutExpo" });
+    }
+
+    function updateWindow() {
+      windowWidth = $window.outerWidth();
+      windowHeight = $window.outerHeight();
+      let holderHeight = sliderWidth - windowWidth + windowHeight;
+      $holder.css("height", holderHeight + "px");
+    }
+
+    $window.on("wheel", handleWheel);
+    $window.on("resize", updateWindow);
+
+    updateWindow();
+  }
+
+  render() {
+    return (
+      <div className="holder">
+        <div className="fixer">
+          <div className="slider">
+            <section className="section"></section>
+            <section className="section"></section>
+            <section className="section"></section>
+            <section className="section"></section>
+            <section className="section"></section>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
