9e202af7e845c1980e4a22669cd929f127d4c6ba
Ben Hays <ben@kajabi.com>
2019-08-02 11:48:09 -0700

    Added Fauxpad.

--- src/components/EmailMarketing.js
+++ src/components/EmailMarketing.js
@@ -4,6 +4,9 @@ const EmailMarketing = () => {
   return (
     <section className="section email-marketing">
       <div className="fauxpad">
+        <div className="fauxpad__button fauxpad__power"></div>
+        <div className="fauxpad__button fauxpad__volume fauxpad__volume--1"></div>
+        <div className="fauxpad__button fauxpad__volume fauxpad__volume--2"></div>
         <div className="fauxpad__overflow">
           <div className="fauxpad__outer-frame"></div>
           <div className="fauxpad__frame"></div>
